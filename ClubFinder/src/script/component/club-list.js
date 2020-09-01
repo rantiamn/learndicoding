6e455ffcb4edf23a9c9b8caa7ae50ca39a3afd17
rantiamn <68527949+rantiamn@users.noreply.github.com>
2020-09-01 13:50:35 +0700

    Update club-list.js

--- ClubFinder/src/script/component/club-list.js
+++ ClubFinder/src/script/component/club-list.js
@@ -1,24 +1,24 @@
 import './club-item.js';
 
-class clubList extends HTMLElement {
-  set clubs(clubs) {
-      this._clubs = clubs;
-      this.render();
-  }
+class ClubList extends HTMLElement {
+    set clubs(clubs) {
+        this._clubs = clubs;
+        this.render();
+    }
 
-  render() {
-    this.innerHTML = "";
-    this._clubs.forEach(club => {
-      const clubItemElement = document.createElement("club-item");
-      clubItemElement.club = club
-      this.appendChild(clubItemElement);
-    })
-  }
-  
+    render() {
+        this.innerHTML = "";
+        this._clubs.forEach(club => {
+            const clubItemElement = document.createElement("club-item");
+            clubItemElement.club = club;
+            this.appendChild(clubItemElement);
+        })
+    }
+
     renderError(message) {
-    this.innerHTML = "";
-    this.innerHTML += '<h2 class="placeholder">${message}</h2>';
-  }
+        this.innerHTML = "";
+        this.innerHTML += `<h2 class="placeholder">${message}</h2>`;
+    }
 }
 
-customElements.define("club-list", clubList);
+customElements.define("club-list", ClubList);
