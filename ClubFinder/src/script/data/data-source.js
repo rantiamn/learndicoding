ae17a89a263367673fa6bd9a6dafe87232db5aef
rantiamn <68527949+rantiamn@users.noreply.github.com>
2020-09-01 10:46:50 +0700

    Update data-source.js

--- ClubFinder/src/script/data/data-source.js
+++ ClubFinder/src/script/data/data-source.js
@@ -1,11 +1,5 @@
 import clubs from './clubs,js'
-const main = function () {
-   const searchElement = document.querySelector("#searchElement");
-   const buttonSearchElement = document.querySelector("#searchButtonElement");
-   const clubListElement = document.querySelector("#clubList");
 
-
-   const onButtonSearchClicked = function () {
        class dataSource {
          static searchClub(keyword) {
            return new Promise((resolve, reject) => {
@@ -18,37 +12,5 @@ const main = function () {
          });
          }
        }
-       const filteredClub = clubs.filter(club => club.name.toUpperCase().includes(keyword.toUpperCase()));
-   };
 
-
-   const renderResult = function (results) {
-       clubListElement.innerHTML = "";
-       results.forEach(function (club) {
-           const name = club.name;
-           const fanArt = club.fanArt;
-           const description = club.description;
-
-
-           const clubElement = document.createElement("div");
-           clubElement.setAttribute("class", "club");
-
-
-           clubElement.innerHTML = '<img class="fan-art-club" src="' + fanArt + '" alt="Fan Art">\n' +
-               '<div class="club-info">\n' +
-               '<h2>' + name + '</h2>\n' +
-               '<p>' + description + '</p>' +
-               '</div>';
-           clubListElement.appendChild(clubElement);
-       })
-   };
-
-
-   const fallbackResult = function (message) {
-       clubListElement.innerHTML = "";
-       clubListElement.innerHTML += '<h2 class="placeholder">' + message + '</h2>'
-   };
-
-
-   buttonSearchElement.addEventListener("click", onButtonSearchClicked);
-};
+ export default DataSource;
